609b9a35297f8c40bac115b764621447ac55a0df
infiniteoo <troydorman@gmail.com>
2023-12-19 13:24:05 -0700

    displaying products in the client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -6,6 +6,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { ProductDisplayComponent } from './product-display/product-display.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +19,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     ToolbarComponent,
     ProductDisplayComponent,
     MatToolbarModule,
+    HttpClientModule,
     MatSidenavModule,
   ],
   templateUrl: './app.component.html',
@@ -24,4 +27,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 })
 export class AppComponent {
   title = 'client';
+
+  @Output() products: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // query the server for the products
+    this.http.get('http://localhost:5000/api/products').subscribe((data) => {
+      console.log(data);
+      this.products = data;
+    });
+  }
 }
--- client/src/app/product-display/product-display.component.ts
+++ client/src/app/product-display/product-display.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-display',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule, MatIconModule],
   templateUrl: './product-display.component.html',
   styleUrl: './product-display.component.css',
 })
-export class ProductDisplayComponent {}
+export class ProductDisplayComponent {
+  @Input() products: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit called'); // Check if ngOnInit is called
+    console.log('products:', this.products); // Check the value of 'products'
+
+    if (this.products) {
+      console.log('products inside product display: ', this.products);
+    }
+  }
+}
