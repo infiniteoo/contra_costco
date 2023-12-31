f329841566edcb4faa241e86935ce7e70f12d3d0
infiniteoo <troydorman@gmail.com>
2023-12-31 14:10:13 -0700

    added price and customer review filters in sidebar

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChunkPipe } from '../../../chunk.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
   standalone: true,
-  imports: [CommonModule, ChunkPipe],
+  imports: [CommonModule, ChunkPipe, FormsModule],
 })
 export class SidebarComponent {
   @Input() products: any[] = [];
+  minPrice: number = 0;
+  maxPrice: number = 100; // Replace with your actual maximum price
 
   removeDuplicates(productTags: string[]): string[] {
     return Array.from(new Set(productTags));
@@ -23,4 +26,16 @@ export class SidebarComponent {
     }
     return result;
   }
+
+  updatePriceRange(event: Event) {
+    // Get the values from the input elements
+    const minValue = Number((event.target as HTMLInputElement).value);
+    const maxValue = Number(this.maxPrice);
+
+    // Update minPrice and maxPrice
+    this.minPrice = minValue;
+    this.maxPrice = maxValue;
+  }
+
+  filterByRating(rating: number) {}
 }
